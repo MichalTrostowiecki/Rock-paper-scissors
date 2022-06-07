74bed794e838ff78f91fd671fae0b45fb7003b6a
MichalTrostowiecki <michal.trostowiecki@gmail.com>
2022-06-07 20:59:34 +0100

    I've created more functions to find a winner of a game

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -1,27 +1,68 @@
 
 
 let myArray = ['rock', 'scissors', 'paper'];
+let playerScore = 0;
+let computerScore =0;
 
 
-
+// This function is producing choice made by computer which is random
 function computerPlay() {
   result = myArray[Math.floor(Math.random() * myArray.length)];
   return result;
 }
 
-const playRound = (playerSelection, computerSelection) => {
-  computerSelection = computerPlay();
-  playerSelection = 'rock';
-  if (playerSelection === 'rock' && computerSelection === 'rock') {
-    console.log(`It's a tie : ${computerSelection} + ${playerSelection}`);
-  } else if 
-  ((computerSelection == "rock" && playerSelection == "scissors") ||
-  (computerSelection == "scissors" && playerSelection == "paper") ||
-  (computerSelection == "paper" && playerSelection == "rock")) {
-    console.log(`You've lost : ${computerSelection} + ${playerSelection}`);
+
+// This function is a logic for paper,scissor rock game
+
+  const playRound = (playerSelection, computerSelection) => {
+    computerSelection = computerPlay();
+    playerSelection = 'rock';
+    playerWon = false;
+    computerWon = false;
+    
+    if (playerSelection === 'rock' && computerSelection === 'rock') {
+      return console.log("it's a tie");
+    } else if 
+    ((computerSelection === "rock" && playerSelection === "scissors") ||
+    (computerSelection === "scissors" && playerSelection === "paper") ||
+    (computerSelection === "paper" && playerSelection === "rock"))
+    {
+      computerScore += 1;
+      computerWon = true;
+      return console.log(`Computer: ${computerSelection} beats Player: ${playerSelection}`);
+
+
+    } else {
+      playerScore += 1;
+      playerWon = true;
+      return console.log(`Player: ${playerSelection} beats Computer: ${computerSelection}`);
+    }
+    
+  }
+
+function game() {
+   for (let i=0; i < 5; i++) {
+    playRound();
+     if (playerWon === true) {
+       console.log(`You've won this round. \nYour points: ${playerScore}  Computer points: ${computerScore}`);
+     } else if ((playerWon === computerWon) && (playerScore === computerScore)) {
+       console.log(`No one wins! \nYour points: ${playerScore}   Computer points: ${computerScore}`);
+     } else if (computerWon === true) {
+      console.log(`You've lost this round. \nYour points: ${playerScore}  Computer points: ${computerScore}`);
+     }
+   }
+}
+
+function endGame() {
+  if (playerScore >= 5 || computerScore >= 5) {
+    if (playerScore < computerScore) {
+      console.log(`Computer wins with: ${computerScore}`);
+    } else if(playerScore > computerScore) {
+      console.log(`Player wins with: ${playerScore}`);
+    }
   } else {
-    console.log('Some error in the logic');
+    game();
   }
 }
 
-playRound();
+endGame();
