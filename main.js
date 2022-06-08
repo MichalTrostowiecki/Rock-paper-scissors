b45542c950fd4a5841421a4054b657febad7302c
MichalTrostowiecki <michal.trostowiecki@gmail.com>
2022-06-08 21:44:15 +0100

    I've added some DOM manipulation using Javascript to create a UI

--- main.js
+++ main.js
@@ -1,3 +1,30 @@
+const container = document.querySelector('#container');
+const div = document.createElement('div');
+const button1 = document.createElement('button');
+button1.textContent = 'Scissors';
+button1.setAttribute('id','btn');
+const button2 = document.createElement('button');
+button2.textContent = 'Rock'
+button2.setAttribute('id','btn');
+const button3 = document.createElement('button');
+button3.textContent = 'Paper';
+button3.setAttribute('id','btn');
+
+div.appendChild(button1);
+div.appendChild(button2);
+div.appendChild(button3);
+container.appendChild(div);
+
+// Creating a function with loop to add event listeners for all my buttons.
+const btn = document.querySelectorAll('#btn').forEach(item =>{
+item.addEventListener('click', (event) => {
+  console.log(event.target);
+})
+});
+
+
+
+
 let playerScore = 0;
 let computerScore =0;
 
@@ -54,20 +81,24 @@ function playRound (playerSelection, computerSelection) {
   }
 }
 
+
+
+
+
 // This function executes 5 rounds of a game
-function game() {
-  for (let i=0; i < 5; i++) {
-    if (playerScore === 5) {
-      console.log(`Player wins by reaching first: ${playerScore} points.`);
-    } else if (computerScore === 5) {
-      console.log(`Computer wins by reaching first: ${computerScore} points.`);
-    } else {
-      playRound();
-    }
-  }
-}
+// function game() {
+//   for (let i=0; i < 5; i++) {
+//     if (playerScore === 5) {
+//       console.log(`Player wins by reaching first: ${playerScore} points.`);
+//     } else if (computerScore === 5) {
+//       console.log(`Computer wins by reaching first: ${computerScore} points.`);
+//     } else {
+//       playRound();
+//     }
+//   }
+// }
 
 
 
-game();
+
 
